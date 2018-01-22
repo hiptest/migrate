136b730b584a7a7f51cb77d2bd6a5c4c4f2e6296
Timothée Ville <timothee.ville@hiptest.net>
2018-01-22 13:58:57 +0100

    Init

---
+++ README.md
@@ -0,0 +1,48 @@
+# Zephyr project migration
+
+## 1. Export Zephyr project.
+
+The only way to retrieve the steps of each test case is to put all of them in a test cycle and to export that test cycle.
+You can do it (bulk) by following these steps :
+
+ 1. Create a dedicated test cycle (called "All Tests" for example).
+ ![new test cycle](https://downloads.intercomcdn.com/i/o/45686453/9afe54ce6ac75bbb21688f37/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.43.07.png)
+ 
+ 2. In the 'Search Test' section, create a search filter ('save as' button at the top of the page) named 'All tests' that only filter the issueType to 'Test': 
+ project=<projectId> AND issuetype=Test
+ ![search tests section](https://downloads.intercomcdn.com/i/o/45686750/3a97f8186b4445cf6266c8b4/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.44.21.png)
+ 
+ ![new search filter](https://downloads.intercomcdn.com/i/o/45687223/04593cbd710b0c9ff2df21fc/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.45.43.png)
+ 
+ 3. Press the 'All Tests' gear in the test cycle list, select "add tests", then enter the filter name 'All tests' in the 'search filter' pane. Validate.
+ ![test cycle list](https://downloads.intercomcdn.com/i/o/45687596/b80b2fdef6c1ea7f1daf9a37/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.48.30.png)
+ 
+ ![add tests bulk](https://downloads.intercomcdn.com/i/o/45687792/ead8a9b0f53d1f0ac8e2c02d/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.50.38.png)
+
+ 4. All your tests are now imported to the 'All Tests' test cycle. You should now be able to export the 'All Tests' test cycle from the 'Search test execution' page.
+
+ ![search test executions](https://downloads.intercomcdn.com/i/o/45688034/083e87926f58f3c3c670d85f/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.52.39.png)
+ 
+ ![select test cycle](https://downloads.intercomcdn.com/i/o/45688531/8c293bbc5817d5532640b55a/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.54.15.png)
+ 
+ ![export test cycle](https://downloads.intercomcdn.com/i/o/45688639/a592178553a2912af84658f9/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+16.55.36.png)
+
+Finally, to have all test informations (like description, etc), could you please try to export all zephyr tests from the 'Search Test' page:
+
+![export search tests](https://downloads.intercomcdn.com/i/o/45689099/1d9fa06201754199006ffb3b/Capture+d%E2%80%99e%CC%81cran+2018-01-17+a%CC%80+15.04.03.png)
+
+## 2. Execute script
+
+Migration script needs some variable to push exported project to Hiptest:
+
+ 1. HT_ACCESS_TOKEN, HT_CLIENT, HT_UID: can be found in   your Hiptest profile page. Once you have them, export them in your shell session
+ 
+ 2. Hiptest can't create a project from API. You will have to create a project from Hiptest application then retrieve your project id in the URL : 'https://hiptest.net/app/projects/&lt;YOUR_PROJECT_ID&gt;/' and export it in the HT_PROJECT variable in your shell session.
+
+Now you could launch the script with: 
+```shell
+./migrate_zephyr.rb export_file1.xml export_file2.xml
+```
+
+with export_file1.xml et export_file2.xml the two previously described Zephyr exports.
+ 
---
+++ migrate_zephyr.rb
@@ -0,0 +1,476 @@
+#!/usr/bin/env ruby
+require 'pry'
+require 'nokogiri'
+require 'singleton'
+require 'net/http'
+require "uri"
+require "json"
+
+TO_TAG_NODES = [:link, :environment, :key, :priority, :status, :fixVersion, :labels, :versions, :issuekey]
+ONLY_KEY_TAGS = []
+HIPTEST_API_URI = 'https://hiptest.net/api'
+
+###########################
+#        CLASSES          #
+###########################
+
+class Project
+  include Singleton
+  
+  attr_accessor :name, :description, :folders, :scenarios
+  
+  def initialize()
+    @name = ''
+    @description = ''
+    @folders = []
+    @scenarios = []
+    @root_folder_id = nil
+  end
+  
+  def api_create_or_update
+    get_root_folder_id
+    @scenarios.each do |scenario|
+      scenario.folder_id = @root_folder_id
+      scenario.api_create_or_update
+    end
+    
+    @folders.each do |folder|
+      folder.parent_id = @root_folder_id
+      folder.api_create_or_update
+    end
+  end
+  
+  def get_root_folder_id
+    uri = URI(HIPTEST_API_URI + "/projects/#{ENV['HT_PROJECT']}/folders")
+    res = get(uri)
+    if res
+      @root_folder_id = res['data'].select { |folder| folder.dig('attributes', 'parent-id').nil? }.first['id']
+    end
+  end
+end
+
+
+class Folder
+  attr_accessor :id, :name, :scenarios, :parent_id, :api_path
+  
+  def initialize(name, scenarios = [])
+    @id = nil
+    @parent_id = nil
+    @name = name
+    @scenarios = scenarios
+    @api_path = HIPTEST_API_URI + "/projects/#{ENV['HT_PROJECT']}/folders"
+    Project.instance.folders << self
+  end
+  
+  def self.find_or_create_by_name(name)
+    folder = Project.instance.folders.select{ |f| f.name == name }.first
+    
+    if folder.nil?
+      folder = Folder.new(name)
+    end
+    
+    folder
+  end
+  
+  def api_create_or_update
+    body = {
+      data: {
+        attributes: {
+          name: @name,
+          "parent-id": @parent_id
+        }
+      } 
+    }
+    
+    create_or_update(self, body, 'folders')
+
+    @scenarios.each do |scenario|
+      scenario.folder_id = @id
+      scenario.api_create_or_update
+    end
+  end
+  
+  def api_exists?
+    uri = URI(@api_path)
+    exists?(self, uri, 'name', @name)
+  end
+end
+
+
+class Scenario
+  @@scenarios = []
+  
+  attr_accessor :id, :name, :project, :description, :steps, :folder, :tags, :folder_id, :api_path
+  
+  def initialize(name, description = '')
+    @id = nil
+    @folder_id = nil
+    @name = name
+    @description = description
+    @steps = []
+    @tags = []
+    @api_path = HIPTEST_API_URI + "/projects/#{ENV['HT_PROJECT']}/scenarios"
+    @@scenarios << self
+  end
+  
+  def definition
+    definition = "scenario '#{@name}' do\n"
+    @steps.each do |step|
+      unless step.dig(:step).empty?
+        action = "action: \"#{step.dig(:step)}\""
+      else
+        action = "result: \"#{step.dig(:result)}\""
+      end
+      definition << "  step {#{action}}\n"
+    end
+    
+    definition << "\nend"
+    definition
+  end
+  
+  def api_create_or_update
+    body = {
+      data: {
+        attributes: {
+          name: @name,
+          description: @description,
+          "folder-id": @folder_id,
+          definition: definition
+        }
+      }
+    }
+    
+    create_or_update(self, body, 'scenarios')
+    
+    @tags.each do |tag|
+      tag.scenario_id = @id
+      tag.api_create_or_update
+    end
+  end
+  
+  def self.find_by_name(name)
+    @@scenarios.select{ |sc| sc.name == name }.first
+  end
+  
+  def self.find(id)
+    @@scenarios.select{ |sc| sc.id == id }.first
+  end
+  
+  def api_exists?
+    uri = URI(HIPTEST_API_URI + "/projects/#{ENV['HT_PROJECT']}/scenarios")
+    exists?(self, uri, 'name', @name)
+  end
+end
+
+
+class Tag
+  attr_accessor :id, :key, :value, :api_path
+  attr_reader :scenario_id
+  
+  def initialize(key, value = '')
+    @id = nil
+    @key = key
+    @value = value
+    @scenario_id = nil
+    @api_path = nil
+  end
+  
+  def scenario_id=(scenario_id)
+    @scenario_id = scenario_id
+    @api_path = HIPTEST_API_URI + "/projects/#{ENV['HT_PROJECT']}/scenarios/#{@scenario_id}/tags"
+  end
+  
+  def api_create_or_update
+    # TODO: GET back to work!
+    body = {
+      data: {
+        attributes: {
+          key: @key,
+          value: @value
+        }
+      }
+    }
+    
+    create_or_update(self, body, 'tags') 
+  end
+  
+  def api_exists?
+    exist = false
+    res = get(URI(@api_path))
+    
+    if res and res['data'].any?
+      res['data'].each do |r|
+        if r.dig('attributes', 'key') == @key.to_s and r.dig('attributes', 'value') == @value
+          exist = true
+          @id = r.dig('id')
+        end
+      end
+    end
+    
+    exist
+  end
+end
+
+
+
+###########################
+#       API REQUESTS      #
+###########################
+
+def get(uri)
+  req = Net::HTTP::Get.new(uri.path)
+  send_request(uri, req)
+end
+
+def post(uri, body)
+  req = Net::HTTP::Post.new(uri.path)
+  req.body = body
+  
+  send_request(uri, req)
+end
+
+def patch(uri, body)
+  req = Net::HTTP::Patch.new(uri.path)
+  req.body = body
+  
+  send_request(uri, req)
+end
+
+def send_request(uri, req)
+  res = nil
+  
+  add_auth_header_to_request(req)
+  response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+    http.request(req)
+  end
+  
+  if response.code == "200"
+    res = JSON.parse(response.body)
+  else
+    puts response.message
+  end
+  
+  res
+end
+
+def exists?(resource, uri, attribute_name, attribute)
+  exist = false
+  res = get(uri)
+  
+  if res and res['data'].any?
+    res['data'].each do |r|
+      if r.dig('attributes', attribute_name) == attribute
+        exist = true
+        resource.id = r.dig('id')
+      end
+    end
+  end
+  
+  exist
+end
+
+def create_or_update(resource, body, resource_type = nil)
+  res = nil
+  
+  if resource.api_exists?
+    resource.api_path += "/#{resource.id}"
+    uri = URI(api_path)
+    body[:data][:type] = resource_type
+    body[:data][:id] = resource.id
+    
+    res = patch(uri, body.to_json)
+  else
+    uri = URI(resource.api_path)
+    res = post(uri, body.to_json)
+    resource.id = res.dig('data', 'id')
+  end
+end
+
+def add_auth_header_to_request(request)
+  headers.each do |name, value|
+    request[name] = value
+  end
+end
+
+def headers
+  env_variables = get_env_variables
+  {
+    "Content-Type" => "application/json",
+    "Accept" => "application/vnd.api+json; version=1",
+    "access-token" => "#{env_variables[:access_token]}",
+    "client" => "#{env_variables[:client]}",
+    "uid" => "#{env_variables[:uid]}"
+  }
+end
+
+def get_env_variables  
+  {
+    access_token: ENV['HT_ACCESS_TOKEN'],
+    client: ENV['HT_CLIENT'],
+    uid: ENV['HT_UID'],
+    project_id: ENV['HT_PROJECT']
+  }
+end
+
+def check_env_variables
+  env_var_names = ['HT_ACCESS_TOKEN', 'HT_CLIENT', 'HT_UID', 'HT_PROJECT']
+  is_errored = false
+  
+  env_var_names.each do |env_var|
+    if ENV[env_var].nil?
+      is_errored = true
+      missing_env(env_var)
+    end
+  end
+  
+  if is_errored
+    puts
+    help
+    exit(1)
+  end
+end
+
+
+###########################
+#        HELPERS          #
+###########################
+
+def missing_env(var_name)
+  puts "#{var_name} environment variable is missing, please export it to push to Hiptest."
+end
+
+def help
+  puts "Usage"
+  puts "./migrate_zephyr.rb file1.xml file2.xml"
+  puts
+  puts "Some environment variables are required to push your project to Hiptest. Please export them in your terminal session or specify them before the script call."
+  puts "\tHT_ACCESS_TOKEN:\tYou may find it in your Hiptest profile page"
+  puts "\tHT_CLIENT:\t\tYou may find it in your Hiptest profile page"
+  puts "\tHT_UID:\t\t\tYou may find it in your Hiptest profile page"
+  puts "\tHT_PROJECT:\t\tYou may find it in the Url of your project. http://hiptest.net/app/projects/<project_id>"
+  puts
+  puts "Example: HT_ACCESS_TOKEN=xxxxxx HT_CLIENT=xxxxxx HT_UID=xxxxxx HT_PROJECT=xxxx ./migrate_zephyr.rb file1.xml file2.xml"
+end
+
+def parse_files(arguments)
+  files = []
+  arguments.each do |path|
+    if File.file?(path) and path.end_with?('.xml')
+      files << File.open(path) { |f| Nokogiri::XML(f)}
+    end
+  end
+  files
+end
+
+def is_info_file? file_nodes
+  file_nodes.xpath('//item').any? and file_nodes.xpath('//item/project').any? and file_nodes.xpath('//item/summary') and file_nodes.xpath('//item/type')[0].text === 'Test'
+end
+
+def is_execution_file? file_nodes
+  file_nodes.xpath('//execution').any? and file_nodes.xpath('//execution/project').any? and file_nodes.xpath('//execution/testSummary').any?
+end
+
+
+def determinate_info_and_execution_files(files)
+  infos, executions = nil
+  
+  files.each do |file|
+    if is_info_file? file
+      infos = file
+    end
+    
+    if is_execution_file? file
+      executions = file
+    end
+  end
+  
+  if infos.nil? or executions.nil?
+    help
+    exit(1)
+  end
+  
+  [infos, executions]
+end
+
+
+
+
+###########################
+#       PROCESSING        #
+###########################
+
+def process_infos(infos_nodes)
+  tests_nodes = infos_nodes.xpath('//item')
+  
+  tests_nodes.each do |test_node|
+    sc = {}
+    test_node.children.each do |child| 
+      sc[child.name.to_sym] = child.content.strip
+    end
+    
+    Project.instance.name = sc[:project]
+    scenario = Scenario.new(sc[:summary], sc[:description])
+    
+    TO_TAG_NODES.each do |tag|
+      unless ONLY_KEY_TAGS.include? tag
+        scenario.tags << Tag.new(tag, sc[tag]) unless sc[tag].nil? or sc[tag].empty?
+      else
+        scenario.tags << Tag.new(tag)
+      end
+    end
+    
+    folder = nil
+    if sc[:component]
+      folder = Folder.find_or_create_by_name(sc[:component])
+      folder.scenarios << scenario
+    else
+      Project.instance.scenarios << scenario
+    end
+  end
+end
+
+
+def process_executions(executions_nodes)
+  tests_nodes = executions_nodes.xpath('//execution')
+  tests_nodes.each do |test_node|
+    execution = {}
+    steps = []
+    
+    test_node.children.each do |child|
+      if child.name == 'teststeps'
+        child.element_children.each do |step_node|
+          test_step = {}
+          step_node.element_children.each do |step_attribute|
+            test_step[step_attribute.name.to_sym] = step_attribute.content.strip
+          end
+          steps << test_step
+        end
+      else
+        execution[child.name.to_sym] = child.content.strip
+      end
+    end
+    
+    scenario = Scenario.find_by_name(execution[:testSummary])
+    scenario.steps = steps
+  end
+end
+
+
+
+###########################
+#           MAIN          #
+###########################
+
+if __FILE__ == $0
+  check_env_variables
+  if ARGV.count == 2
+    files = parse_files(ARGV)
+    infos, executions = determinate_info_and_execution_files(files)
+    process_infos(infos)
+    process_executions(executions)
+    Project.instance.api_create_or_update
+  else
+    help
+    exit(1)
+  end
+end
