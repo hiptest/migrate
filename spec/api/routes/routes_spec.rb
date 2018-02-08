53c025fb6acb3a8806d1c4ebd60ea91552b04ce5
Timothée Ville <timothee.ville@hiptest.net>
2018-02-08 15:53:28 +0100

    Oops

---
+++ spec/api/routes/routes_spec.rb
@@ -0,0 +1,50 @@
+require './lib/api/api'
+require 'webmock/rspec'
+
+RSpec.describe API::Hiptest, 'API Scenarios' do
+  let!(:api){
+    API::Hiptest.configure do |config|
+      config.access_token = "access_token"
+      config.client = "client"
+      config.uid = "uid@uid.uid"
+    end
+    
+    API::Hiptest.new
+  }
+  
+  let(:auth_headers) {
+    {
+      "accept": "application/vnd.api+json; version=1",
+      "access-token": API::Hiptest.configuration.access_token,
+      "client": API::Hiptest.configuration.client,
+      "uid": API::Hiptest.configuration.uid
+    }
+  }
+  
+  before do
+    @project_id = 1
+    @folder_id = 1
+  end
+  
+  context "routing system" do
+    
+    it "raises an error when resource is not defined in routes" do
+      expect {
+        api.get_bidibou(@project_id, @bidibou_id)
+      }.to raise_error(RuntimeError, "Resource 'bidibou' is not found")
+    end
+    
+    it "raises an error when action resource is not defined in routes" do
+      expect {
+        api.create_folder_tag(@project_id, @folder_id, {})
+      }.to raise_error(RuntimeError, "Route 'create' not found for folder tag")
+    end
+    
+    it "raises an error when the method has no sense" do
+      expect {
+        api.culture(@project_id, @folder_id, {})
+      }.to raise_error(RuntimeError, "The method 'culture' doesn't exist or isn't implemented yet")
+    end
+    
+  end
+end
