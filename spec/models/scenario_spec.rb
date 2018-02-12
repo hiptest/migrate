df0d3f914335c0c8e2ad99fb8dfdea46482823c5
Vincent Pretre <vincent.pretre@gmail.com>
2018-02-12 14:10:37 +0100

    Add tests to ensure scenarios are not created with an existing name

--- spec/models/scenario_spec.rb
+++ spec/models/scenario_spec.rb
@@ -5,7 +5,7 @@ require './spec/models/models_shared'
 describe Models::Scenario do
   it_behaves_like 'a model' do
     let(:api){ double("API::Hiptest") }
-    
+
     let(:an_existing_object ) {
       sc = Models::Scenario.new('My first scenario')
       sc.jira_id='PLOP-1'
@@ -20,10 +20,10 @@ describe Models::Scenario do
     let(:resource_id) { 1664 }
 
     let(:find_url) {'https://hiptest.net/api/projects/1/scenarios/find_by_tags'}
-    
+
     let(:query_that_found) { '?key=JIRA&value=PLOP-1' }
     let(:query_that_not_found) { '?key=JIRA&value=PLOP-2' }
-    
+
     let(:create_url) {'https://hiptest.net/api/projects/1/scenarios'}
     let(:update_url) { "https://hiptest.net/api/projects/1/scenarios/#{resource_id}" }
 
@@ -38,8 +38,8 @@ describe Models::Scenario do
         }
       }
     }
-    
-    let(:update_data) { 
+
+    let(:update_data) {
       {
         data: {
           id: '1664',
@@ -106,7 +106,7 @@ describe Models::Scenario do
       it 'checks that the scenario name is the beginning of the returned result' do
         allow(api).to receive(:get).with(URI(find_url)).and_return(find_results)
         scenario.class.api = api
-        
+
         expect(scenario.api_exists?).to be true
         expect(scenario.id).to eq(find_data.first['id'])
       end
@@ -142,24 +142,24 @@ describe Models::Scenario do
       it 'uses the first result which name starts with the scenario name' do
         allow(api).to receive(:get).with(URI(find_url)).and_return(find_results)
         scenario.class.api = api
-        
+
         expect(scenario.api_exists?).to be true
         expect(scenario.id).to eq(find_data[1]['id'])
       end
     end
   end
-  
+
   context "when saving the first time" do
     let(:api){ double("API::Hiptest") }
     let(:create_url) {'https://hiptest.net/api/projects/1/scenarios'}
     let(:find_url) { "#{create_url}/find_by_tags?key=JIRA&value=PLOP-1" }
-    
+
     let(:scenario ) {
       sc = Models::Scenario.new('My first scenario')
       sc.jira_id='PLOP-1'
       sc
     }
-    
+
     let(:create_data) {
       {
         data: {
@@ -171,7 +171,7 @@ describe Models::Scenario do
         }
       }
     }
-    
+
     let(:created_data){
       {
         'type' => 'scenarios',
@@ -181,18 +181,70 @@ describe Models::Scenario do
         }
       }
     }
-    
+
     it "creates the scenario then updates it with its definition" do
       allow(api).to receive(:get).with(URI(create_url)).and_return({ 'data' => []})
       allow(api).to receive(:get).with(URI(find_url)).and_return({ 'data' => []})
       allow(api).to receive(:post).with(URI(create_url), create_data).and_return(created_data)
       scenario.class.api = api
-      
+
       allow(scenario).to receive(:update)
       expect(scenario.api_exists?).to be false
-      
+
       scenario.save
       expect(scenario).to have_received(:update)
+    end
+  end
+
+  context "scenarios are renamed before saving" do
+    let(:api){ double("API::Hiptest") }
+    let(:create_url) {'https://hiptest.net/api/projects/1/scenarios'}
+    let(:find_url) { "#{create_url}/find_by_tags?key=JIRA&value=PLOP-1" }
+
+    let(:scenario ) {
+      sc = Models::Scenario.new('My scenario')
+      sc.jira_id='PLOP-1'
+      sc
+    }
+
+    let(:create_data) {
+      {
+        data: {
+          attributes: {
+            name: 'My scenario (4)',
+            description: "",
+            "folder-id": nil
+          }
+        }
+      }
+    }
+
+    let(:created_data){
+      {
+        'type' => 'scenarios',
+        'id' => '1664',
+        'attributes' => {
+          'name' => 'My scenario (4)'
+        }
+      }
+    }
+
+    it "a unique name is found based on the existing ones" do
+      allow(api).to receive(:get).with(URI(create_url)).and_return({ 'data' => [
+        {'type' => 'scenarios', 'attributes' => {'name' => 'My scenario'}},
+        {'type' => 'scenarios', 'attributes' => {'name' => 'My scenario (1)'}},
+        {'type' => 'scenarios', 'attributes' => {'name' => 'My scenario (2)'}},
+        {'type' => 'scenarios', 'attributes' => {'name' => 'My scenario (3)'}},
+        {'type' => 'scenarios', 'attributes' => {'name' => 'My scenario (5)'}},
+      ]})
+      allow(api).to receive(:get).with(URI(find_url)).and_return({ 'data' => []})
+      allow(api).to receive(:post).with(URI(create_url), create_data).and_return(created_data)
+      scenario.class.api = api
+
+      allow(scenario).to receive(:update)
+
+      scenario.save
+      expect(scenario.name).to eq('My scenario (4)')
     end
   end
 end
