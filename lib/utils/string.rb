fa049ae4c0d63f2621261c081ab40f7df49e41e5
Vincent Pretre <vincent.pretre@gmail.com>
2018-02-19 15:16:54 +0100

    Fix issue with Unicode quotes

--- lib/utils/string.rb
+++ lib/utils/string.rb
@@ -4,35 +4,35 @@ class String
   def is_plural?
     self == self.pluralize
   end
-  
+
   def as_enum_lines
     self.gsub(/\s([\d]{2,}\.[\D])/, "\n\\1")
   end
-  
+
   def single_quotes_escaped
-    self.gsub("'", %q(\\\'))
+    self.gsub("'", %q(\\\')).gsub('&#x27;', %q(\\\'))
   end
-  
+
   def double_quotes_escaped
     self.gsub('"', %Q(\\\"))
   end
-  
+
   def single_quotes_replaced
-    self.gsub("'", '"')
+    self.gsub("'", '"').gsub('&#x27;', '"')
   end
-  
+
   def double_quotes_replaced
     self.gsub('"', "'")
   end
-  
+
   def html_escaped
     self.gsub(%r{&#\w[\d]+;}, '')
   end
-  
+
   def tag_escaped
     self.gsub(%r{</?[^>]+?>}, '').gsub(%r{&lt;/?[^(&gt;)]+?&gt;}, '')
   end
-  
+
   def safe
     self.tag_escaped.html_escaped
   end
--- spec/utils/string_spec.rb
+++ spec/utils/string_spec.rb
@@ -3,37 +3,45 @@ describe String do
     it "return true if self is plural" do
       expect('scenarios'.is_plural?).to be_truthy
     end
-    
+
     it "return false if self is not plural" do
       expect('senario'.is_plural?).not_to be_truthy
     end
   end
-  
+
   context '#as_enum_list' do
     it "transform string into enumerated list" do
       expect('01. blabla 02. blibli 03. bloubl... 04. Plopinette'.as_enum_lines)
         .to eq("01. blabla\n02. blibli\n03. bloubl...\n04. Plopinette")
     end
   end
-  
+
   context '#single_quotes_escaped' do
     it "escape single quotes" do
       expect("Say 'hello'".single_quotes_escaped).to eq %q(Say \\'hello\\')
     end
+
+    it 'also work with unicode expressed single quotes' do
+      expect("Do this by selecting &#x27;Tech_Checks&#x27;".single_quotes_escaped).to eq %q(Do this by selecting \\'Tech_Checks\\')
+    end
   end
-  
+
   context '#double_quotes_escaped' do
     it "escape double quotes" do
       expect('Say "hello"'.double_quotes_escaped).to eq "Say \\\"hello\\\""
     end
   end
-  
+
   context '#single_quotes_replaced' do
     it "replace single quotes by double quotes" do
       expect(%q(Say 'hello').single_quotes_replaced).to eq %q(Say "hello")
     end
+
+    it "also works with single quotes expressed as unicode" do
+      expect("Do this by selecting &#x27;Tech_Checks&#x27;".single_quotes_replaced).to eq %q(Do this by selecting "Tech_Checks")
+    end
   end
-  
+
   context '#double_quotes_replaced' do
     it "replace double quotes by single quotes" do
       expect(%Q(Say "hello").double_quotes_replaced).to eq %Q(Say 'hello')
