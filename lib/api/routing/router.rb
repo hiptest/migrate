b7b713ff56cc4fbfdbbb8181335cc46e146da17b
Timothée Ville <timothee.ville@hiptest.net>
2018-02-27 18:09:14 +0100

    [ API ] Extract a route object to handle routing
    Instead of using a hash, so we can have specialized method and remove a
    bunch of duplicated code.

--- lib/api/routing/router.rb
+++ lib/api/routing/router.rb
@@ -11,42 +11,32 @@ module API
   module Routing
     include API::Routing::Routes
 
-    def magic_calculation(resource_type, parent_type: nil)
-      parent_type = parent_type || @@routes.dig(resource_type.singularize.to_sym, :parent)
-      grand_parent_type = @@routes.dig(parent_type, :parent)
-      [grand_parent_type&.to_s, parent_type&.to_s]
-    end
-
     def method_missing(name, *args)
       action, resource_type = name.to_s.split('_', 2)
-      unless API::Routing::Routes.exists?(resource_type)
-        if resource_type
-          raise RuntimeError.new("Resource '#{resource_type}' is not found")
-        else
-          raise RuntimeError.new("The method '#{name}' doesn't exist or isn't implemented yet")
-        end
-      end
+      raise RuntimeError.new("The method '#{name}' doesn't exist or isn't implemented yet") unless resource_type
+      route = API::Routing::Routes.lookup(resource_type) || raise(RuntimeError.new("Resource '#{resource_type}' is not found"))
 
-      grand_parent_type, parent_type = magic_calculation(resource_type, parent_type: parent_type)
+      grand_parent_type = route.grand_parent_type
+      parent_type = route.parent_type
 
-      available_routes = @@routes.dig(resource_type.singularize.to_sym, :only)
-
-      resource_name = @@routes.dig(resource_type.to_s.singularize.to_sym, :key)&.to_s || resource_type
-
-      if !available_routes.include?(action.to_sym) && action.to_sym != :get
-        subject = [grand_parent_type, parent_type, resource_name].compact.join(' ')
+      if !route.allowed?(action)
+        subject = [grand_parent_type, parent_type, route.data_type].compact.join(' ')
         raise RuntimeError.new("Route '#{action}' not found for #{subject}")
       end
 
       case action
       when 'get'
-        dispatch_get(args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
+        if resource_type.is_plural?
+          dispatch_index(route, args: args)
+        else
+          dispatch_show(route, args: args)
+        end
       when 'create'
-        dispatch_create(args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
+        dispatch_create(route, args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
       when 'update'
-        dispatch_update(args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
+        dispatch_update(route, args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
       when 'delete'
-        dispatch_delete(args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
+        dispatch_delete(route, args: args, resource_type: resource_type, parent_type: parent_type, grand_parent_type: grand_parent_type)
       end
     end
 
@@ -56,46 +46,61 @@ module API
 
     private
 
-    def dispatch_get(args:, resource_type:, parent_type:, grand_parent_type:)
+    def dispatch_index(route, args:)
       grand_parent_id = nil
       parent_id = nil
 
       project_id = args[0]
 
-      if grand_parent_type
+      if route.grand_parent_type
         grand_parent_id = args[1]
         parent_id = args[2]
-      elsif parent_type
+      elsif route.parent_type
         parent_id = args[1]
       end
 
       url = build_url(
         project_id: project_id,
-        resource_type: resource_type,
+        resource_type: route.data_type,
         parent_id: parent_id,
-        parent_type: parent_type,
+        parent_type: route.parent_type,
         grand_parent_id: grand_parent_id,
-        grand_parent_type: grand_parent_type
+        grand_parent_type: route.grand_parent_type
       )
 
-      if resource_type.is_plural?
-        get(URI(url))
-      else
-        if grand_parent_type
-          resource_id = args[3]
-        elsif parent_type
-          resource_id = args[2]
-        else
-          resource_id = args[1]
-        end
-
-        url << "/#{resource_id}"
-
-        get(URI(url))
-      end
+      get(URI(url))
     end
 
-    def dispatch_create(args:, resource_type:, parent_type:, grand_parent_type:)
+    def dispatch_show(route, args:)
+      grand_parent_id = nil
+      parent_id = nil
+
+      project_id = args[0]
+
+      if route.grand_parent_type
+        grand_parent_id = args[1]
+        parent_id = args[2]
+        resource_id = args[3]
+      elsif route.parent_type
+        parent_id = args[1]
+        resource_id = args[2]
+      else
+        resource_id = args[1]
+      end
+
+      url = build_url(
+        project_id: project_id,
+        resource_type: route.data_type,
+        parent_id: parent_id,
+        parent_type: route.parent_type,
+        grand_parent_id: grand_parent_id,
+        grand_parent_type: route.grand_parent_type
+      ) + "/#{resource_id}"
+
+      get(URI(url))
+    end
+
+    def dispatch_create(route, args:, resource_type:, parent_type:, grand_parent_type:)
       grand_parent_id = nil
       parent_id = nil
 
@@ -124,7 +129,7 @@ module API
       post(URI(url), data)
     end
 
-    def dispatch_update(args:, resource_type:, parent_type:, grand_parent_type:)
+    def dispatch_update(route, args:, resource_type:, parent_type:, grand_parent_type:)
       grand_parent_id = nil
       parent_id = nil
 
@@ -156,7 +161,7 @@ module API
       patch(URI(url), data)
     end
 
-    def dispatch_delete(args:, resource_type:, parent_type:, grand_parent_type:)
+    def dispatch_delete(route, args:, resource_type:, parent_type:, grand_parent_type:)
       grand_parent_id = nil
       parent_id = nil
 
--- lib/api/routing/routes.rb
+++ lib/api/routing/routes.rb
@@ -1,45 +1,101 @@
 module API::Routing::Routes
-  @@routes = {
-    actionword: {
+
+  class Route
+    attr_reader :name, :only, :parent_type, :data_type
+    def initialize(name:, only:, parent_type: nil, data_type: nil)
+      @name = name
+      @only = only
+      @parent_type = parent_type
+      @data_type = data_type || name
+    end
+
+    def allowed?(action)
+      if action.to_sym == :get
+        only.include?(:show) || only.include?(:index)
+      else
+        only.include?(action.to_sym)
+      end
+    end
+
+    def parent_route
+      API::Routing::Routes.lookup(parent_type)
+    end
+
+    def grand_parent_type
+      parent_route&.parent_type
+    end
+  end
+
+  ROUTES = [
+    Route.new(
+      name: "actionword",
       only: [:show, :index, :create, :update, :delete],
-    },
-    scenario: {
+    ),
+    Route.new(
+      name: "scenario",
       only: [:show, :index, :create, :update, :delete],
-    },
-    parameter: {
-      parent: :scenario,
+    ),
+    Route.new(
+      name: "parameter",
+      parent_type: "scenario",
       only: [:show, :index, :create, :update, :delete],
-    },
-    dataset: {
-      parent: :scenario,
+    ),
+    Route.new(
+      name: "dataset",
+      parent_type: "scenario",
       only: [:show, :index, :create, :update, :delete],
-    },
-    folder: {
+    ),
+    Route.new(
+      name: "folder",
       only: [:show, :index, :create, :update, :delete],
-    },
-    testRun: {
+    ),
+    Route.new(
+      name: "testRun",
       only: [:show, :index, :create],
-    },
-    testSnapshot: {
-      parent: :testRun,
+    ),
+    Route.new(
+      name: "testSnapshot",
+      parent_type: "testRun",
       only: [:show, :index],
-    },
-    testResult: {
-      parent: :testSnapshot,
+    ),
+    Route.new(
+      name: "testResult",
+      parent_type: "testSnapshot",
       only: [:create],
-    },
-    scenarioTag: {
-      key: :tag,
-      parent: :scenario,
+    ),
+    Route.new(
+      name: "scenarioTag",
+      data_type: "tag",
+      parent_type: "scenario",
       only: [:show, :index, :create, :update, :delete],
-    },
-    folderTag: {
-      key: :tag,
-      parent: :folder,
+    ),
+    Route.new(
+      name: "folderTag",
+      data_type: "tag",
+      parent_type: "folder",
       only: [:index],
-    },
-  }
+    ),
+  ]
+
+  @@routes = {}
+  @@routes_index = {}
+  ROUTES.each do |route|
+    route_info = {
+      parent: route.parent_type,
+      only: route.only,
+      key: route.data_type,
+    }
+    @@routes[route.name.to_sym] = route_info
+    @@routes_index[route.name.to_s.singularize] = route
+    @@routes_index[route.name.to_s.pluralize] = route
+    @@routes_index[route.name.to_sym] = route
+  end
+
   class << self
+    def lookup(route_name)
+      @@routes_index[route_name]
+    end
+
     def level(route_name)
       return 0 unless route_name
       route_name = route_name.to_s.split('_').last.singularize.to_sym
