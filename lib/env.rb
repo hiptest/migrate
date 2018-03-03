d4f593a77de5d7d1e04d2e1a495c8bc67eec5013
Christophe Bliard <christophe.bliard@trux.info>
2018-03-03 10:18:33 +0100

    Inline helpers because there are used only once

--- lib/env.rb
+++ lib/env.rb
@@ -1,5 +1,3 @@
-require './lib/helpers'
-
 def get_env_variables
   {
     access_token: ENV['HT_ACCESS_TOKEN'],
@@ -16,13 +14,22 @@ def check_env_variables
   env_var_names.each do |env_var|
     if ENV[env_var].nil?
       is_errored = true
-      missing_env(env_var)
+      puts "#{var_name} environment variable is missing, please export it to push to Hiptest."
     end
   end
 
   if is_errored
     puts
-    help
+    puts "Usage"
+    puts "./migrate_zephyr.rb file1.xml file2.xml"
+    puts
+    puts "Some environment variables are required to push your project to Hiptest. Please export them in your terminal session or specify them before the script call."
+    puts "\tHT_ACCESS_TOKEN:\tYou may find it in your Hiptest profile page"
+    puts "\tHT_CLIENT:\t\tYou may find it in your Hiptest profile page"
+    puts "\tHT_UID:\t\t\tYou may find it in your Hiptest profile page"
+    puts "\tHT_PROJECT:\t\tYou may find it in the Url of your project. http://hiptest.net/app/projects/<project_id>"
+    puts
+    puts "Example: HT_ACCESS_TOKEN=xxxxxx HT_CLIENT=xxxxxx HT_UID=xxxxxx HT_PROJECT=xxxx ./migrate_zephyr.rb file1.xml file2.xml"
     exit(1)
   end
 end
